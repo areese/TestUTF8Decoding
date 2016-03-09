dc26a8e54c33376fda788bbfe20605eb88626bb9
Allen Reese <areese@yahoo-inc.com>
2016-03-09 14:57:24 -0800

    Be jdk6 friendly

--- src/main/java/com/yahoo/example/testutf8/TestUTF8.java
+++ src/main/java/com/yahoo/example/testutf8/TestUTF8.java
@@ -4,6 +4,11 @@ package com.yahoo.example.testutf8;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.CharacterCodingException;
+import java.nio.charset.CharsetEncoder;
+import java.nio.charset.StandardCharsets;
 
 public class TestUTF8 {
     public static void main(String[] args) throws Exception {
@@ -11,20 +16,42 @@ public class TestUTF8 {
                         "%E6%8C%91%E6%88%B0%E5%85%A8%E5%8F%B0%E6%9C%80%E4%BD%8E%E5%83%B9%F0%9F%8D%85%E4%BA%BA%E6%B0%A3";
         String decoded = URLDecoder.decode(encoded, "UTF-8");
 
+        testUTF8Decoder(decoded);
         testJava(decoded);
         testJNI(decoded);
     }
 
-    private static void testJNI(String decoded) throws UnsupportedEncodingException {
+    private static void testJNI(String decoded) {
+        JNITest.dumpDecodedBytes(decoded);
+    }
+
+    private static void testJava(String decoded) throws UnsupportedEncodingException {
         byte[] raw = decoded.getBytes("UTF-8");
         System.out.println("java");
         for (byte b : raw) {
-            System.out.print(Integer.toHexString(Byte.toUnsignedInt(b)));
+            System.out.print(Integer.toHexString((((int) b) & 0x00FF)));
         }
         System.out.println();
     }
 
-    private static void testJava(String utf8encoded) {
-        JNITest.dumpDecodedBytes(utf8encoded);
+    private static void testUTF8Decoder(String decoded) throws UnsupportedEncodingException, CharacterCodingException {
+        CharsetEncoder encoder = StandardCharsets.UTF_8.newEncoder();
+
+        CharBuffer in = CharBuffer.allocate(decoded.length());
+        in.put(decoded, 0, decoded.length());
+        in.flip();
+        ByteBuffer encoded = encoder.encode(in);
+        if (!encoded.hasArray()) {
+            System.out.println("java charset adsadsadsaadfss");
+
+        }
+
+        System.out.println("java charset encoder");
+        encoded.flip();
+        byte[] array = encoded.array();
+        for (byte b : encoded.array()) {
+            System.out.print(Integer.toHexString((((int) b) & 0x00FF)));
+        }
+        System.out.println();
     }
 }
