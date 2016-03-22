dd860c99f2dae21c61195fb455704f9b8646389a
Allen Reese <areese@yahoo-inc.com>
2016-03-22 09:00:55 -0700

    Add SampleInfo class to make example testing easier, this is kinda close to what this is really intended to generate code for. Add more missing license headers

---
+++ src/main/java/com/yahoo/example/test/SampleInfo.java
@@ -0,0 +1,101 @@
+// Copyright 2016 Yahoo Inc.
+// Licensed under the terms of the New-BSD license. Please see LICENSE file in the project root for terms.
+package com.yahoo.example.test;
+
+import java.net.InetAddress;
+
+public final class SampleInfo {
+
+    private final int type;
+    private final int attrs;
+    private final int status;
+
+    private final long expiration;
+    private final int readCount;
+    private final int writeCount;
+    private final long writeTimestamp;
+
+    private final InetAddress ia;
+
+    private final String org;
+    private final String loc;
+    private final String ccode;
+    private final String desc;
+
+
+    public SampleInfo(int type, int attrs, int status, long expiration, int readCount, int writeCount,
+                    long writeTimestamp, InetAddress ia, String org, String loc, String ccode, String desc) {
+        this.type = type;
+        this.attrs = attrs;
+        this.status = status;
+        this.expiration = expiration;
+        this.readCount = readCount;
+        this.writeCount = writeCount;
+        this.writeTimestamp = writeTimestamp;
+        this.ia = ia;
+        this.org = org;
+        this.loc = loc;
+        this.ccode = ccode;
+        this.desc = desc;
+    }
+
+
+    public int getType() {
+        return type;
+    }
+
+
+    public int getAttrs() {
+        return attrs;
+    }
+
+
+    public int getStatus() {
+        return status;
+    }
+
+
+    public long getExpiration() {
+        return expiration;
+    }
+
+
+    public int getReadCount() {
+        return readCount;
+    }
+
+
+    public int getWriteCount() {
+        return writeCount;
+    }
+
+
+    public long getWriteTimestamp() {
+        return writeTimestamp;
+    }
+
+
+    public InetAddress getIa() {
+        return ia;
+    }
+
+
+    public String getOrg() {
+        return org;
+    }
+
+
+    public String getLoc() {
+        return loc;
+    }
+
+
+    public String getCcode() {
+        return ccode;
+    }
+
+
+    public String getDesc() {
+        return desc;
+    }
+}
