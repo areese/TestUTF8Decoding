e3e1bb7bfd0f2bb6eaaa6e15943338c211e3a453
Allen Reese <areese@yahoo-inc.com>
2016-04-22 10:07:38 -0700

    Commit the right version of the test

--- src/test/java/com/yahoo/wildwest/TestFingersArentLost.java
+++ src/test/java/com/yahoo/wildwest/TestFingersArentLost.java
@@ -12,7 +12,6 @@ public class TestFingersArentLost {
         InetAddress address = InetAddress.getByName("192.168.1.254");
         MissingFingers powersaw = InetAddressAccessor.powersaw(address);
         Assert.assertEquals(powersaw.getLength(), 0);
-        Assert.assertEquals(powersaw.getAddress(), false);
         Assert.assertEquals(powersaw.getAddress(), 0xc0a801fe);
     }
 
@@ -20,7 +19,6 @@ public class TestFingersArentLost {
     public void ipv6() throws UnknownHostException {
         InetAddress address = InetAddress.getByName("[2001:db8:85a3:8d3:1319:8a2e:370:7348]");
         MissingFingers powersaw = InetAddressAccessor.powersaw(address);
-        Assert.assertEquals(powersaw.getAddress(), true);
         Assert.assertEquals(powersaw.getLength(), 16);
         Assert.assertNotEquals(powersaw.getAddress(), 0);
     }
