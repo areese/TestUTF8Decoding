e2d604d9ed848c043298dcc396cc831f9766446a
Allen Reese <areese@yahoo-inc.com>
2016-05-04 12:33:23 -0700

    Continue mucking around to get jmh working when built with gradle

---
+++ src/test/java/com/yahoo/jmh/jmhtest/RunBenchmark.java
@@ -0,0 +1,12 @@
+// Copyright 2016 Yahoo Inc.
+// Licensed under the terms of the New-BSD license. Please see LICENSE file in the project root for terms.
+package com.yahoo.jmh.jmhtest;
+
+import org.openjdk.jmh.Main;
+
+public class RunBenchmark {
+
+    public static void main(String... args) throws Exception {
+        Main.main(args);
+    }
+}
--- src/test/java/com/yahoo/test/strings/PerfStringReturns.java
+++ src/test/java/com/yahoo/test/strings/PerfStringReturns.java
@@ -1,15 +1,21 @@
 package com.yahoo.test.strings;
 
+import java.io.IOException;
+
 import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.CompilerControl;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.runner.RunnerException;
 
 import com.yahoo.example.testutf8.JniLibraryLoader;
 
 @State(Scope.Benchmark)
 public class PerfStringReturns {
+    public static void main(String... args) throws RunnerException, IOException {
+        org.openjdk.jmh.Main.main(args);
+    }
+
     @Setup
     public void setup() {
         JniLibraryLoader.load();
